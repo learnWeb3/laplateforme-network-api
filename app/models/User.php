668f7a631f250f5d23e1d8bf5c345821148a2a57
antoine-tech <antoinehadrienleguillou@outlook.com>
2021-01-04 21:30:23 +0100

    removing offset condition on getFeedContent

--- app/models/User.php
+++ app/models/User.php
@@ -143,7 +143,7 @@ class User extends Application
         OR id_user=?
         ORDER BY posts.created_at DESC LIMIT $limit OFFSET $offset";
         $results = Request::send($connection, $request_body, [$this->id, $this->id])->fetchAll(PDO::FETCH_ASSOC);
-        if (empty($results) && $offset == 0) {
+        if (empty($results)) {
             return Post::all($connection, $endpoint_path, $offset, $limit);
         } else {
             $next_start = $offset += 10;
